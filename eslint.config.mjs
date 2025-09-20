d6b2a2fdd77a4de8b014a5cc31ae7e5bb262854f
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-09-20 19:25:59 +1000

    chore: init

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended", eslintConfigPrettier],
+    languageOptions: { globals: globals.browser },
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+]);
