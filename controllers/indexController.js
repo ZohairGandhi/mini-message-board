3113483185ae95f5438bff4e68c8c1af9cbc1355
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-09-28 00:42:16 +1000

    refactor: extract header & footer to partial views & add footer info

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,13 @@
 const msgsStorage = require("../storages/msgsStorage");
 
 exports.indexGet = (req, res) =>
-  res.render("index", { messages: msgsStorage.getMsgs() });
+  res.render("index", {
+    messages: msgsStorage.getMsgs(),
+    title: "Mini Message Board",
+  });
 
-exports.indexNewGet = (req, res) => res.render("form");
+exports.indexNewGet = (req, res) =>
+  res.render("form", { title: "Add New Message" });
 
 exports.indexNewPost = (req, res) => {
   const { text, user } = req.body;
@@ -13,5 +17,8 @@ exports.indexNewPost = (req, res) => {
 
 exports.indexMessagesGet = (req, res) => {
   const id = req.params.msgIdx;
-  res.render("message", { message: msgsStorage.getMsg(id) });
+  res.render("message", {
+    message: msgsStorage.getMsg(id),
+    title: "View Message",
+  });
 };
