68df4527a7460135d544854e3ec0783fa93b51c9
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2025-09-27 23:47:46 +1000

    feat: add handler for /messages/:idx route & fix CSS issue
    Fix CSS path in link tags by adding preceding '/'.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,5 @@
+const msgsStorage = require("../storages/msgsStorage");
+
 exports.indexGet = (req, res) =>
   res.render("index", { messages: msgsStorage.getMsgs() });
 
@@ -7,4 +9,9 @@ exports.indexNewPost = (req, res) => {
   const { text, user } = req.body;
   msgsStorage.addMsg({ text, user, added: new Date() });
   res.redirect("/");
+};
+
+exports.indexMessagesGet = (req, res) => {
+  const id = req.params.msgIdx;
+  res.render("message", { message: msgsStorage.getMsg(id) });
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,5 +5,6 @@ const indexRouter = Router();
 indexRouter.get("/", indexController.indexGet);
 indexRouter.get("/new", indexController.indexNewGet);
 indexRouter.post("/new", indexController.indexNewPost);
+indexRouter.get("/messages/:msgIdx", indexController.indexMessagesGet);
 
 module.exports = indexRouter;
